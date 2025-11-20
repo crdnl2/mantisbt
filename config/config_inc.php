427a653d34cbe07e000d6ceab1840c6cd8fd25b7
Rohit <rohitp4all@gmail.com>
2025-11-20 12:56:12 +0530

    config_inc: more string changes
    Signed-off-by: Rohit <rohitp4all@gmail.com>

--- config/config_inc.php
+++ config/config_inc.php
@@ -1,32 +1,35 @@
 <?php
 
-$g_status_enum_string = '';
-$g_status_enum_string = '10:open,20:review,30:assigned,40:wip,50:on-hold,60:resolved,80:closed';
+/* STATUS LIST */
+$g_status_enum_string = '10:open,20:review,30:wip,40:onhold,50:assigned,60:resolved,80:closed';
 
+/* WORKFLOW */
 $g_status_enum_workflow = array(
-    10 => '20:review,30:assigned,40:wip,50:on-hold,60:resolved',
-    20 => '30:assigned,40:wip,50:on-hold,60:resolved',
-    30 => '40:wip,50:on-hold,60:resolved',
-    40 => '50:on-hold,60:resolved',
-    50 => '40:wip,60:resolved',
-    60 => '80:closed,40:wip',
+    10 => '20:review,30:wip,40:onhold,50:assigned,60:resolved',
+    20 => '30:wip,40:onhold,50:assigned,60:resolved',
+    30 => '40:onhold,50:assigned,60:resolved',
+    40 => '50:assigned,60:resolved',
+    50 => '30:wip,40:onhold,60:resolved',
+    60 => '80:closed,40:onhold',
     80 => ''
 );
 
+/* COLORS */
 $g_status_colors = array(
     'open'     => '#fcbdbd',
     'review'   => '#fff2a8',
-    'assigned' => '#c2dfff',
-    'wip'      => '#ffd5a3',
-    'on-hold'  => '#e3e3e3',
+    'wip'      => '#c2dfff',
+    'onhold'   => '#ffd5a3',
+    'assigned' => '#e3e3e3',
     'resolved' => '#d2f5b0',
     'closed'   => '#c9ccc4'
 );
 
-/* REQUIRED thresholds */
-$g_status_fixed_threshold = 60;            // Resolved starts at 60
+/* THRESHOLDS */
+$g_status_fixed_threshold = 60;
 $g_bug_resolved_status_threshold = 60;
 $g_bug_closed_status_threshold = 80;
+
 
 /* Reopen behavior | This forces reopened tickets to fall under open status */
 $g_bug_reopen_status = 10;
