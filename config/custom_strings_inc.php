76e8285f613f9567e9dd78c5a6bf5da55a469cf0
Rohit <rohitp4all@gmail.com>
2025-11-20 13:15:30 +0530

    mantis: fix bug_title and bug_button
    This commit resolves APPLICATION WARNING #300: String "wip_bug_title" not found error since we have reset the status.
    
    Signed-off-by: Rohit <rohitp4all@gmail.com>

--- config/custom_strings_inc.php
+++ config/custom_strings_inc.php
@@ -2,5 +2,22 @@
 
 $s_status_enum_string = '10:open,20:review,30:wip,40:onhold,50:assigned,60:resolved,80:closed';
 
+# Custom status display names
+$s_open_bug_title      = 'Open Issue';
+$s_review_bug_title    = 'Under Review';
+$s_assigned_bug_title  = 'Assigned';
+$s_wip_bug_title       = 'Work in Progress';
+$s_onhold_bug_title    = 'On Hold';
+$s_resolved_bug_title  = 'Resolved';
+$s_closed_bug_title    = 'Closed';
+
+# Button labels (optional but recommended)
+$s_open_bug_button      = 'Open';
+$s_review_bug_button    = 'Review';
+$s_assigned_bug_button  = 'Assign';
+$s_wip_bug_button       = 'Set WIP';
+$s_onhold_bug_button    = 'Put On Hold';
+$s_resolved_bug_button  = 'Resolve';
+$s_closed_bug_button    = 'Close';
 
 ?>
